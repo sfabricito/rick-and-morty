c9eed34155d094feaacb621fffe30bad5e3ee955
sfabricito <fabriciosolisalpizar@gmail.com>
2022-04-15 19:29:42 -0600

    Creation of the templates and pages of the site

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+const Home = () => {
+    const view = `
+    <article class="cards">
+        <div class="Status">
+            <span title="Status"></span>
+        </div>
+        <img src="Image" alt="Image of the character Name">
+        <div class="info">
+            <h3>Name</h3>
+            <p>Origin</p>
+        </div>
+    </article>
+    `
+}
